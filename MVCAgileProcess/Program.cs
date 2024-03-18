8e6f619f77ad14fafc58bd7198c36676cbdf5950
Kwok Yuk Chui <kchui8@my.centennialcollege.ca>
2024-03-17 22:54:24 -0400

    add a model

--- MVCAgileProcess/Program.cs
+++ MVCAgileProcess/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVCAgileProcess.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MVCAgileProcessContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MVCAgileProcessContext") ?? throw new InvalidOperationException("Connection string 'MVCAgileProcessContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
