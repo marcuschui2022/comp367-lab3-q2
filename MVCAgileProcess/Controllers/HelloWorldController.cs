1969dfa4c15869d8dadf713d653d31fc85fc7236
Kwok Yuk Chui <kchui8@my.centennialcollege.ca>
2024-03-17 22:45:57 -0400

    add a view

--- MVCAgileProcess/Controllers/HelloWorldController.cs
+++ MVCAgileProcess/Controllers/HelloWorldController.cs
@@ -5,23 +5,30 @@ namespace MVCAgileProcess.Controllers
 {
     public class HelloWorldController : Controller
     {
-        //public IActionResult Index()
-        // {
-        //     return View();
-        // }
+        public IActionResult Index()
+         {
+             return View();
+         }
+         
+         public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
         // 
         // GET: /HelloWorld/
-        public string Index()
-        {
-            return "This is my default action...";
-        }
+       // public string Index()
+       // {
+        //    return "This is my default action...";
+        //}
         // 
         // GET: /HelloWorld/Welcome/ 
         // GET: /HelloWorld/Welcome/ 
         // Requires using System.Text.Encodings.Web;
-        public string Welcome(string name, int numTimes = 1)
-        {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
-        }
+        // public string Welcome(string name, int numTimes = 1)
+        // {
+        //     return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        // }
     }
 }
