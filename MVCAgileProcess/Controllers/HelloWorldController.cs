719a2c926c38c4e828544851337444596e8dcc18
Kwok Yuk Chui <kchui8@my.centennialcollege.ca>
2024-03-17 22:30:52 -0400

    add a controller

---
+++ MVCAgileProcess/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCAgileProcess.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //public IActionResult Index()
+        // {
+        //     return View();
+        // }
+        // 
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
